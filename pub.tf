a6ff18c9c93712ad72b4a18835f62dcb4447a9e3
Lakshay Arora <53274714+lakshayarora476@users.noreply.github.com>
2020-12-23 17:41:04 +0530

    Create pub.tf

---
+++ pub.tf
@@ -0,0 +1,15 @@
+resource "random_string" "Terraformfqdn" {
+ length  = 6
+ special = false
+ upper   = false
+ number  = false
+}
+
+resource "azurerm_public_ip" "TerraformPublicIp" {
+ name                         = join("-",[var.public_ip_name, random_string.Terraformfqdn.result])
+ location                     = var.public_ip_location
+ resource_group_name          = var.public_ip_rg_name
+ allocation_method = var.public_ip_allocation_method
+ domain_name_label            = random_string.Terraformfqdn.result
+ tags                         = var.tags
+}
