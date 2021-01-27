c3ba3102d16a877c234c533844677ee08cd6486d
Lakshay Arora <53274714+lakshayarora476@users.noreply.github.com>
2021-01-27 14:51:08 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+#############################
+#Provider
+#############################
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.44.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
